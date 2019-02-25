3879fd6193fd0ed94a29b28e7a5d4100436f5fc9
TowhidKashem <tkashem@gmail.com>
2019-02-25 15:54:53 -0500

    work

---
+++ public/src/js/feed.js
@@ -0,0 +1,65 @@
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector(
+  '#close-create-post-modal-btn'
+);
+var sharedMomentsArea = document.querySelector('#shared-moments');
+
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+
+  if (defferedPrompt) {
+    defferedPrompt.prompt();
+
+    defferedPrompt.userChoice.then(choiceResult => {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to homescreen');
+      }
+    });
+
+    defferedPrompt = null;
+  }
+}
+
+function createCard() {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.style.color = '#fff';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    createCard();
+  });
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
---
+++ public/sw.js
@@ -0,0 +1,83 @@
+const STATIC_VERSION = 'v7';
+const DYNAMIC_VERSION = 'v7';
+
+self.addEventListener('install', event => {
+  console.log('[SW] Installing Service Worker...', event);
+
+  event.waitUntil(
+    caches.open(`static-${STATIC_VERSION}`).then(cache => {
+      console.log('[SW] Precaching App Shell');
+
+      // Cache App Shell
+      cache.addAll([
+        // HTML
+        '/',
+        '/index.html',
+        // JS
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        // CSS
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        // Images
+        '/src/images/main-image.jpg',
+        // External
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[SW] Activating Service Worker...', event);
+
+  // Delete outdated caches
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (
+            key !== `static-${STATIC_VERSION}` &&
+            key !== `dynamic-${DYNAMIC_VERSION}`
+          ) {
+            console.log('[SW] Removing Old Cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  return self.clients.claim();
+});
+
+// Intercept all fetch requests (this will get triggered not just for fetch API calls from JS but also all images, CSS, JS, etc loaded from HTML)
+self.addEventListener('fetch', event => {
+  // console.log('[SW] Fetching Something...', event);
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // If resource exists in cache return cached version
+      if (response) {
+        return response;
+      }
+      // Otherwise fetch it from the network as usual
+      else {
+        return fetch(event.request)
+          .then(response => {
+            return caches.open(`dynamic-${DYNAMIC_VERSION}`).then(cache => {
+              // After fetching store in cache
+              // .put() unlike .add() doesn't make a new request, it simply adds what you already have to the cache
+              // Must use .clone(), response is empty because they can only be used once so you must return a copy
+              cache.put(event.request.url, response.clone());
+              return response;
+            });
+          })
+          .catch(err => {});
+      }
+    })
+  );
+});
