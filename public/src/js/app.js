8f37ab4a48e096b382b4dfbca468780db59f5ca5
TowhidKashem <tkashem@gmail.com>
2019-02-28 09:54:16 -0500

    work

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -7,9 +7,37 @@ if ('serviceWorker' in navigator) {
     .catch(error => console.error(error));
 }
 
+// Delaying add to homepage banner
 window.addEventListener('beforeinstallprompt', event => {
   console.log('beforeinstallprompt fired');
   event.preventDefault();
   defferedPrompt = event;
   return false;
 });
+
+// Notifications
+function displayConfirmNotification() {
+  new Notification('Successfully Subscribed!');
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(result => {
+    console.log('User Choice:', result);
+
+    if (result !== 'granted') {
+      console.log('No notification permission granted!');
+    } else {
+      console.log('User choice is granted!');
+      displayConfirmNotification();
+    }
+  });
+}
+
+if ('Notification' in window) {
+  const notificationBtns = document.querySelectorAll('.enable-notifications');
+
+  notificationBtns.forEach(btn => {
+    btn.classList.add('show');
+    btn.addEventListener('click', askForNotificationPermission);
+  });
+}
